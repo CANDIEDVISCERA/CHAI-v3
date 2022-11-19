b8dff8937ad69313af8df3270c330d0b72b85c62
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-18 23:36:15 -0800

    Update gifs.js

--- commands/gifs.js
+++ commands/gifs.js
@@ -9,9 +9,9 @@ module.exports = {
 			.setDescription('The gif category')
 			.setRequired(true)
 			.addChoices(
-				{ name: 'one', value: 'https://media4.giphy.com/media/MEdRTaYvZBlVMEjwZ3/200w.gif?cid=6c09b952fmip8ir5ashvge5bqft7naf0kpw6lmmfo7i5ewty&rid=200w.gif&ct=v' },
-				{ name: 'two', value: 'https://media4.giphy.com/media/MEdRTaYvZBlVMEjwZ3/200w.gif?cid=6c09b952fmip8ir5ashvge5bqft7naf0kpw6lmmfo7i5ewty&rid=200w.gif&ct=v' },
-				{ name: 'three', value: 'https://media4.giphy.com/media/MEdRTaYvZBlVMEjwZ3/200w.gif?cid=6c09b952fmip8ir5ashvge5bqft7naf0kpw6lmmfo7i5ewty&rid=200w.gif&ct=v' },
+				{ name: 'one', value: 'gif_dog' },
+				{ name: 'two', value: 'gif_dog' },
+				{ name: 'three', value: 'gif_dog' },
 ))
 
 
