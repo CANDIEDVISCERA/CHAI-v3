86db4770d70ce997942867088bf7a19c8944b437
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-19 00:10:52 -0800

    Update gifs.js

--- commands/gifs.js
+++ commands/gifs.js
@@ -2,21 +2,17 @@ const { SlashCommandBuilder } = require('discord.js');
 const data = new SlashCommandBuilder()
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('echo')
-		.setDescription('Replies with your input!')
-		.addStringOption(option =>
-		option.setName('input')
-			.setDescription('The input to echo back')
-			// Ensure the text will fit in an embed description, if the user chooses that option
-			.setMaxLength(2000))
-	.addChannelOption(option =>
-		option.setName('channel')
-			.setDescription('The channel to echo into')
-			// Ensure the user can only select a TextChannel for output
-			.addChannelTypes(ChannelType.GuildText))
-	.addBooleanOption(option =>
-		option.setName('embed')
-			.setDescription('Whether or not the echo should be embedded'))
+	.setName('info')
+	.setDescription('Get info about a user or a server!')
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('user')
+			.setDescription('Info about a user')
+			.addUserOption(option => option.setName('target').setDescription('The user')))
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('server')
+			.setDescription('Info about the server'));
 
 			};
 			
