cb73a32277b47f4b52133247d05e801d42c18a36
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-18 23:34:32 -0800

    Update gifs.js

--- commands/gifs.js
+++ commands/gifs.js
@@ -9,9 +9,9 @@ module.exports = {
 			.setDescription('The gif category')
 			.setRequired(true)
 			.addChoices(
-				{ name: 'Funny', value: 'gif_funny' },
-				{ name: 'Meme', value: 'gif_meme' },
-				{ name: 'Movie', value: 'gif_movie' },
+				{ name: 'one', value: 'https://media4.giphy.com/media/MEdRTaYvZBlVMEjwZ3/200w.gif?cid=6c09b952fmip8ir5ashvge5bqft7naf0kpw6lmmfo7i5ewty&rid=200w.gif&ct=v' },
+				{ name: 'two', value: 'https://media4.giphy.com/media/MEdRTaYvZBlVMEjwZ3/200w.gif?cid=6c09b952fmip8ir5ashvge5bqft7naf0kpw6lmmfo7i5ewty&rid=200w.gif&ct=v' },
+				{ name: 'three', value: 'https://media4.giphy.com/media/MEdRTaYvZBlVMEjwZ3/200w.gif?cid=6c09b952fmip8ir5ashvge5bqft7naf0kpw6lmmfo7i5ewty&rid=200w.gif&ct=v' },
 ))
 
 
