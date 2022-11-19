84ae30fdddaa1614975743c19c6018a4fd025769
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-18 22:58:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -34,15 +34,5 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-const wait = require('node:timers/promises').setTimeout;
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	if (interaction.commandName === 'ping') {
-		await interaction.deferReply();
-		await wait(4000);
-		await interaction.editReply('Pong!');
-	}
-});
 client.login(token);
