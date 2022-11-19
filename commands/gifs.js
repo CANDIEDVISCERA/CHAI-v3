43533c37112a34efebb173fa6a84533737d2f46b
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-19 00:06:09 -0800

    Update gifs.js

--- commands/gifs.js
+++ commands/gifs.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 const data = new SlashCommandBuilder()
 module.exports = {
-	.setName('echo')
-	.setDescription('Replies with your input!')
-	.addStringOption(option =>
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('Replies with your input!')
+		.addStringOption(option =>
 		option.setName('input')
 			.setDescription('The input to echo back')
 			// Ensure the text will fit in an embed description, if the user chooses that option
