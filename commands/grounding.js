594bc699ca4f6acfdfee69539e383337aaded2f1
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-18 22:37:17 -0800

    Update grounding.js

--- commands/grounding.js
+++ commands/grounding.js
@@ -1,17 +1,28 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
+const { ActionRowBuilder, Events, SelectMenuBuilder } = require('discord.js');
 
-client.on(Events.Interaction.client, async interaction => {
+client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	if (interaction.commandName === 'button') {
+	if (interaction.commandName === 'ping') {
 		const row = new ActionRowBuilder()
 			.addComponents(
-				new ButtonBuilder()
-					.setCustomId('primary')
-					.setLabel('Click me!')
-					.setStyle(ButtonStyle.Primary),
+				new SelectMenuBuilder()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions(
+						{
+							label: 'Select me',
+							description: 'This is a description',
+							value: 'first_option',
+						},
+						{
+							label: 'You can select me too',
+							description: 'This is also a description',
+							value: 'second_option',
+						},
+					),
 			);
 
-		await interaction.reply({ content: 'I think you should,', components: [row] });
+		await interaction.reply({ content: 'Pong!', components: [row] });
 	}
 });
