4a2978c2b80f558daa231ad1095d1e3c51e7137d
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-18 22:54:09 -0800

    Update grounding.js

--- commands/grounding.js
+++ commands/grounding.js
@@ -1,11 +1,7 @@
-const wait = require('node:timers/promises').setTimeout;
-
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	if (interaction.commandName === 'ping') {
-		await interaction.deferReply();
-		await wait(4000);
-		await interaction.editReply('Pong!');
+		await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
 	}
 });
