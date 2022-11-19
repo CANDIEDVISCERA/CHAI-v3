77c01b24be9c8397f686846344c36ad093b31c98
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-18 23:19:41 -0800

    Update gifs.js

--- commands/gifs.js
+++ commands/gifs.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
 	.setName('gif')
 	.setDescription('Sends a random gif!')
 	.addStringOption(option =>
@@ -12,5 +11,6 @@ module.exports = {
 				{ name: 'Funny', value: 'gif_funny' },
 				{ name: 'Meme', value: 'gif_meme' },
 				{ name: 'Movie', value: 'gif_movie' },
-		},
 			));
+	},
+};
